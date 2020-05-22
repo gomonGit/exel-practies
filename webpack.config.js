985c9275204a0b5aa8ef0e771e889c07d9b3190f
Александр Гомон <29gomon@mail.ru>
2020-05-22 19:31:09 +0300

    Добавление плагинов

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,42 @@
-'use strict'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'), // путь к основной папке где будет все приложение
+    mode: "development", // режим разработки
+
+    entry: {
+        main: './index.js'
+    }, // входные точки для приложения
+    output: {
+        filename: 'bundle[hash].js', // где будут находиться все js скрипты
+        path: path.resolve(__dirname, 'dist'), // куда складывать
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                },
+            ],
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "bundle[hash].css"
+        })
+    ]
+}
