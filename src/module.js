68b6c11bdda5c1152e41740fbb52940230058416
Александр Гомон <29gomon@mail.ru>
2020-05-22 20:06:01 +0300

    Настройка лоадеров

--- src/module.js
+++ src/module.js
@@ -1,1 +1,9 @@
 console.log('Module.js')
+
+async function start() {
+
+    await Promise.resolve()
+
+}
+
+start();
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const path = require('path');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'), // путь к основной папке где будет все приложение
-    mode: "development", // режим разработки
+    mode: 'development', // режим разработки
 
     entry: {
         main: './index.js'
     }, // входные точки для приложения
     output: {
-        filename: 'bundle[hash].js', // где будут находиться все js скрипты
+        filename: 'bundle.[hash].js', // где будут находиться все js скрипты
         path: path.resolve(__dirname, 'dist'), // куда складывать
     },
     resolve: {
@@ -36,7 +36,31 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: "bundle[hash].css"
+            filename: 'bundle.[hash].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    }
 }
